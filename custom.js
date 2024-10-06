1786da3f979debf209a50341c3eea89ab690513f
aatish jawalkar <37588527+aatishmj@users.noreply.github.com>
2024-10-06 21:15:42 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,67 @@
+$(window).load(function(){
+    $('.preloader').fadeOut(1000);    
+});
+
+
+$(document).ready(function() {
+
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    $(function() {
+        $('.navbar-default a').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 49
+            }, 1000);
+            event.preventDefault();
+        });
+    });
+
+
+  $(function() {
+    $('body').vegas({
+        slides: [
+            { src: 'images/slide-img1.jpg' },
+            { src: 'images/slide-img2.jpg' },
+            { src: 'images/slide-img3.jpg' }
+        ],
+        timer: false,
+        transition: [ 'zoomIn', ],
+        animation: ['kenburns']
+    });
+  });
+
+
+  $(document).ready(function() {
+      $("#team-carousel").owlCarousel({
+          items : 3,
+          itemsDesktop : [1199,3],
+          itemsDesktopSmall : [979,3],
+          slideSpeed: 300,
+          itemsDesktop : [1199,2],
+          itemsTablet: [768,1],
+          itemsTabletSmall: [985,2],
+          itemsMobile : [479,1],
+      });
+    });
+    
+
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 200) {
+          $('.go-top').fadeIn(200);
+            } else {
+                $('.go-top').fadeOut(200);
+           }
+        });   
+        $('.go-top').click(function(event) {
+          event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, 300);
+    });
+
+
+  new WOW({ mobile: false }).init();
+
+  });
+
